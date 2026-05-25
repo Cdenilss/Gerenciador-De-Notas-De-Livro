c3185d3d7df2ee66bf032c18794e3e0203cffb3c
Carlos Denil Soares e Silva <denil@MacBook-Air-Denil.local>
2026-05-25 17:32:44 -0300

    feat: enforce unique livro ISBN index

---
+++ GerenciadorDeLivro.Infrastructure/Persistence/Migrations/20260514214634_AddUniqueIndexLivroISBN.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciadorDeLivro.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexLivroISBN : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Livros_Titulo",
+                table: "Livros");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_ISBN",
+                table: "Livros",
+                column: "ISBN",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Livros_ISBN",
+                table: "Livros");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_Titulo",
+                table: "Livros",
+                column: "Titulo",
+                unique: true);
+        }
+    }
+}
